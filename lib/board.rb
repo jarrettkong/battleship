d5e5f64991df70c2705b753ef7eb41f2fbf868db
Jarrett Kong <jarrettkong@me.com>
2019-06-11 21:18:31 -0600

    Add diagonal method for board validation

--- lib/board.rb
+++ lib/board.rb
@@ -6,27 +6,30 @@ class Board
   attr_accessor :cells
 
   def initialize
-    @cells = {
-      'A1' => Cell.new('A1'),
-      'A2' => Cell.new('A2'),
-      'A3' => Cell.new('A3'),
-      'A4' => Cell.new('A4'),
-      'B1' => Cell.new('B1'),
-      'B2' => Cell.new('B2'),
-      'B3' => Cell.new('B3'),
-      'B4' => Cell.new('B4'),
-      'C1' => Cell.new('C1'),
-      'C2' => Cell.new('C2'),
-      'C3' => Cell.new('C3'),
-      'C4' => Cell.new('C4'),
-      'D1' => Cell.new('D1'),
-      'D2' => Cell.new('D2'),
-      'D3' => Cell.new('D3'),
-      'D4' => Cell.new('D4')
-    }
+    @cells = {}
+
+    @keys = %w[A1 A2 A3 A4 B1 B2 B3 B4 C1 C2 C3 C4 D1 D2 D3 D4].freeze
+    @keys.map { |cell| @cells[cell] = Cell.new(cell) }
   end
 
   def valid_coordinate?(coordinate)
-    @cells.any? { |cell, _| cell == coordinate }
+    @cells.keys.include?(coordinate)
+  end
+
+  def valid_placement?(ship, coordinates)
+    return false if ship.length != coordinates.length
+    return false if coordinates.uniq.length != coordinates.length
+    return false if diagonal?(coordinates)
+    return false if overlap?(ship, coordinates)
+    true
+  end
+
+  def diagonal?(coordinates)
+    # valid_letters = ('A'..'D').to_a
+    # valid_numbers = (1..4).to_a
+    letters = coordinates.map { |coor| coor[0] }
+    numbers = coordinates.map { |coor| coor[1] }
+    
+    true
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -18,9 +18,9 @@ class BoardTest < Minitest::Test
 
   def test_it_has_cells
     assert_instance_of Hash, @board.cells
-    @board.cells.each_value do |value|
-      assert_instance_of Cell, value
-    end
+    @board.cells.each_value { |value| assert_instance_of Cell, value }
+    assert_equal @board.cells.keys.include?('A1'), true
+    assert_equal @board.cells.keys.include?('D4'), true
   end
 
   def test_valid_coordinate
@@ -31,6 +31,11 @@ class BoardTest < Minitest::Test
   def test_valid_placement_length
     assert_equal @board.valid_placement?(@cruiser, %w[A1 A2]), false
     assert_equal @board.valid_placement?(@submarine, %w[A1 A3 A4]), false
+  end
+
+  def test_valid_placement_unique
+    assert_equal @board.valid_placement?(@cruiser, %w[B3 C3 C3]), false
+    assert_equal @board.valid_placement?(@submarine, %w[A1 A1]), false
   end
 
   def test_valid_placement_consecutive
@@ -48,5 +53,13 @@ class BoardTest < Minitest::Test
   def test_valid_placement_true
     assert_equal @board.valid_placement?(@submarine, %w[A1 A2]), true
     assert_equal @board.valid_placement?(@cruiser, %w[B1 C1 D1]), true
+  end
+
+  def test_diagonal
+    assert_equal @board.diagonal?(%w[A1 B2 C3]), true
+    assert_equal @board.diagonal?(%w[C2 D3]), true
+    assert_equal @board.diagonal?(%w[A1 A2 A3]), false
+    assert_equal @board.diagonal?(%w[A1 A2 B3]), false
+    assert_equal @board.diagonal?(%w[A1 B1 C1]), false
   end
 end
