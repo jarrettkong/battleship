e66949e024ec0a8a308d8e7cf295b851fb81ba1e
Jarrett Kong <jarrettkong@me.com>
2019-06-15 11:15:41 -0400

    Add size to board

--- lib/board.rb
+++ lib/board.rb
@@ -5,7 +5,8 @@ require_relative './cell'
 class Board
   attr_accessor :cells
 
-  def initialize
+  def initialize(size = 4)
+    @size = size
     @cells = {}
 
     @keys = %w[A1 A2 A3 A4 B1 B2 B3 B4 C1 C2 C3 C4 D1 D2 D3 D4].freeze
